fde195cb368c2a6633af36753e4afd704ec7db98
bkeener <bkeener7@gmail.com>
2022-09-26 08:45:36 -0600

    Finish I3

--- lib/curator.rb
+++ lib/curator.rb
@@ -14,4 +14,35 @@ class Curator
     def add_artist(artist)
         artists.push(artist)
     end
+
+    def photographs_by_artist
+        hash = Hash.new {|h,k| h[k] = [] }
+        artists.each do |artist|
+            photographs.each do |photo|
+                if photo.artist_id == artist.id
+                    hash[artist] << photo
+                end
+            end
+        end
+        hash
+    
+    end
+
+    def artists_with_multiple_photographs
+       [] << photographs_by_artist.max_by { |k, v| v.length }.first.name
+    end
+
+    def photographs_taken_by_artist_from(location)
+        artists_from(location).map { |artist| artist.drop(1) }.flatten
+
+    end
+
+    def artists_from(location)
+        photographs_by_artist.select do |artist|
+            artist.country == location
+        end
+    end
+   
+    
+    
 end
--- spec/curator_spec.rb
+++ spec/curator_spec.rb
@@ -17,6 +17,18 @@ RSpec.describe Curator do
         artist_id: "2",      
         year: "1941"      
         })}
+    let(:photo_3) {Photograph.new({    
+        id: "3",      
+        name: "Identical Twins, Roselle, New Jersey",      
+        artist_id: "3",      
+        year: "1967"      
+        })}
+    let(:photo_4) {Photograph.new({    
+        id: "4",      
+        name: "Monolith, The Face of Half Dome",      
+        artist_id: "3",      
+        year: "1927"      
+        })}
     let(:artist_1) {Artist.new({
         id: "1",      
         name: "Henri Cartier-Bresson",      
@@ -29,6 +41,13 @@ RSpec.describe Curator do
         name: "Ansel Adams",      
         born: "1902",      
         died: "1984",      
+        country: "United States"      
+        })}
+    let(:artist_3) {Artist.new({    
+        id: "3",      
+        name: "Diane Arbus",      
+        born: "1923",      
+        died: "1971",      
         country: "United States"      
         })}
 
@@ -51,6 +70,54 @@ RSpec.describe Curator do
         curator.add_artist(artist_1)
         curator.add_artist(artist_2)
         expect(curator.artists).to include artist_1, artist_2
+    end
+
+    it '5. can find photographs by artist' do
+        curator.add_artist(artist_1)
+        curator.add_artist(artist_2)
+        curator.add_artist(artist_3)
+        curator.add_photograph(photo_1)
+        curator.add_photograph(photo_2)
+        curator.add_photograph(photo_3)
+        curator.add_photograph(photo_4)
+        expect(curator.photographs_by_artist).to eq({
+            artist_1 => [photo_1],
+            artist_2 => [photo_2],
+            artist_3 => [photo_3, photo_4]
+            })
+    end
+
+    it '6. can find artists with multiple photographs' do
+        curator.add_artist(artist_1)
+        curator.add_artist(artist_2)
+        curator.add_artist(artist_3)
+        curator.add_photograph(photo_1)
+        curator.add_photograph(photo_2)
+        curator.add_photograph(photo_3)
+        curator.add_photograph(photo_4)
+        expect(curator.artists_with_multiple_photographs).to eq(["Diane Arbus"])
+    end
+
+    it '7. can find photographs from certain places' do
+        curator.add_artist(artist_1)
+        curator.add_artist(artist_2)
+        curator.add_artist(artist_3)
+        curator.add_photograph(photo_1)
+        curator.add_photograph(photo_2)
+        curator.add_photograph(photo_3)
+        curator.add_photograph(photo_4)
+        expect(curator.photographs_taken_by_artist_from("United States")).to eq([photo_2, photo_3, photo_4])
+    end
+
+    it '8. returns empty array if no artists from country' do
+        curator.add_artist(artist_1)
+        curator.add_artist(artist_2)
+        curator.add_artist(artist_3)
+        curator.add_photograph(photo_1)
+        curator.add_photograph(photo_2)
+        curator.add_photograph(photo_3)
+        curator.add_photograph(photo_4)
+        expect(curator.photographs_taken_by_artist_from("Argentina")).to eq([])
     end
 
 end
